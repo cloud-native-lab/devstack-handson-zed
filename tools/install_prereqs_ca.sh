8378d926b1b666c4bfa3586d4d96db1f22361b1b
makocchi-git <makocchi@gmail.com>
2023-01-17 20:12:49 +0900

    update

--- tools/install_prereqs_ca.sh
+++ tools/install_prereqs_ca.sh
@@ -4,62 +4,78 @@ sudo apt-get install -y apache2 augeas-lenses autoconf automake autopoint autoto
 bridge-utils libxen-dev qemu-slof qemu-system-arm qemu-system-mips qemu-system-misc qemu-system-ppc qemu-system-s390x qemu-system-sparc \
 bcache-tools bind9-host binfmt-support build-essential libvirt-daemon libvirt-clients libvirt-daemon-system libvirt-dev \
 comerr-dev conntrack conntrackd cpio cpp cpu-checker dstat eject socat memcached \
-rabbitmq-server sharutils vim unzip sysfsutils python3-distutils python3-dev python-pip-whl pcp
+rabbitmq-server sharutils vim unzip sysfsutils python3-distutils python3-dev python-pip-whl pcp \
+debootstrap ovn-central ovn-common ovn-controller-vtep ovn-host
 
 sudo rm -rf /usr/lib/python3/dist-packages/PyYAML-*.egg-info
 
 # for Zed
-# TODO
-
-# for Victoria
-#sudo /usr/local/bin/pip install \
-#cliff==3.1.0 \
-#cryptography==2.9 \
-#libvirt-python===6.1.0 \
-#numpy==1.18.2 \
-#os-testr==2.0.0 \
-#osc-lib==2.0.0 \
-#oslo.cache==2.3.0 \
-#oslo.concurrency==4.0.2 \
-#oslo.config==8.0.2 \
-#oslo.context==3.0.2 \
-#oslo.db==8.1.0 \
-#oslo.i18n==4.0.1 \
-#oslo.log==4.1.3 \
-#oslo.messaging==12.1.4 \
-#oslo.middleware==4.0.2 \
-#oslo.policy==3.1.1 \
-#oslo.privsep==2.1.2 \
-#oslo.reports==2.0.1 \
-#oslo.rootwrap==6.0.2 \
-#oslo.serialization==3.1.1 \
-#oslo.service==2.1.1 \
-#oslo.upgradecheck==1.0.1 \
-#oslo.utils==4.1.2 \
-#oslo.versionedobjects==2.0.2 \
-#oslo.vmware==3.3.1 \
-#python-barbicanclient==4.10.0  \
-#python-blazarclient==3.0.1 \
-#python-cinderclient==7.0.0 \
-#python-designateclient==4.0.0 \
-#python-glanceclient==3.1.2 \
-#python-heatclient==2.1.0 \
-#python-ironicclient==4.1.0 \
-#python-keystoneclient==4.0.0 \
-#python-magnumclient==3.0.1 \
-#python-manilaclient==2.1.0 \
-#python-mistralclient==4.0.1 \
-#python-monascaclient==2.1.0 \
-#python-neutronclient==7.1.1 \
-#python-novaclient==17.0.0 \
-#python-octaviaclient==2.0.1 \
-#python-openstackclient==5.2.0 \
-#python-saharaclient==3.1.0 \
-#python-swiftclient==3.9.0 \
-#python-troveclient==3.3.2 \
-#python-vitrageclient==4.0.1 \
-#python-zaqarclient==1.13.1 \
-#python-memcached==1.59 \
-#PyMySQL==0.9.3 \
-#PyYAML==5.3.1 \
-#stevedore==1.32.0
+sudo /usr/bin/pip install \
+cliff==4.0.0 \
+cryptography==36.0.2 \
+libvirt-python==6.1.0 \
+numpy==1.23.2 \
+os-brick==6.1.0 \
+os-client-config==2.1.0 \
+os-ken==2.5.0 \
+os-resource-classes==1.1.0 \
+os-service-types==1.7.0 \
+os-testr==3.0.0 \
+os-traits==2.9.0 \
+os-vif==2.8.0 \
+os-win==5.7.1 \
+osc-lib==2.6.2 \
+osc-placement==4.0.0 \
+oslo.cache==3.2.0 \
+oslo.concurrency==5.0.1 \
+oslo.config==9.0.0 \
+oslo.context==5.0.0 \
+oslo.db==12.1.0 \
+oslo.i18n==5.1.0 \
+oslo.limit==2.0.1 \
+oslo.log==5.0.0 \
+oslo.messaging==14.0.0 \
+oslo.metrics==0.5.0 \
+oslo.middleware==5.0.0 \
+oslo.policy==4.0.0 \
+oslo.privsep==3.0.1 \
+oslo.reports==2.4.0 \
+oslo.rootwrap==6.3.1 \
+oslo.serialization==5.0.0 \
+oslo.service==3.0.0 \
+oslo.upgradecheck==2.0.0 \
+oslo.utils==6.0.1 \
+oslo.versionedobjects==3.0.1 \
+oslo.vmware==4.0.1 \
+osprofiler==3.4.3 \
+ovs==2.17.1.post1 \
+wheel==0.34.2 \
+PyMySQL==1.0.2 \
+PyYAML==6.0 \
+python-barbicanclient==5.4.0 \
+python-blazarclient==3.5.0 \
+python-cinderclient==9.1.0 \
+python-dateutil==2.8.2 \
+python-debian==0.1.36ubuntu1 \
+python-designateclient==5.0.0 \
+python-glanceclient==4.1.0 \
+python-heatclient==3.1.0 \
+python-ironicclient==5.0.1 \
+python-keystoneclient==5.0.1 \
+python-magnumclient==4.0.0 \
+python-manilaclient==4.1.1 \
+python-memcached==1.59 \
+python-mistralclient==4.5.0 \
+python-monascaclient==2.6.0 \
+python-neutronclient==8.1.0 \
+python-novaclient==18.1.0 \
+python-octaviaclient==3.1.0 \
+python-openstackclient==6.0.0 \
+python-saharaclient==4.0.2 \
+python-subunit==1.4.0 \
+python-swiftclient==4.1.0 \
+python-troveclient==8.0.0 \
+python-vitrageclient==4.6.0 \
+python-zaqarclient==2.4.0 \
+python-zunclient==4.5.0 \
+stevedore==4.0.2
